6ea366258619e21032b8d50586227e3217a23860
Shashwat Mishra <saamac7777@gmail.com>
2024-10-11 15:12:18 +0530

    ...

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -11,7 +11,7 @@ export default async function Hero() {
         <input
           type="search"
           className="border border-gray-400 w-full py-2 px-3 rounded-md"
-          placeholder="Search phrase.." />
+          placeholder="Search phrase..." />
         <button className="bg-blue-600 text-white py-2 px-4 rounded-md">
           Search
         </button>
